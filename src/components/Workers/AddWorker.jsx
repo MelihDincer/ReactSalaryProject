a089e19f0862873ddaebd7259bc8a4675b673ea1
Melih DİNÇER <m_dincer41@hotmail.com>
2025-01-05 17:30:41 +0300

    Bölüm 9: Fragments, Portals ve Refs kullanımı tamamlandı.

--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,67 +1,88 @@
-import { useState } from "react"
-import Button from "../UI/Button"
-import Card from "../UI/Card"
+import { useState, Fragment, useRef } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddWorker = (props) => {
-  const [enteredWorkerName, setEnteredWorkerName] = useState("");
-  const [enteredWage, setEnteredWage] = useState("");
   const [error, setError] = useState();
-  const minimumWage = 15000; 
+  const nameInputRef = useRef();
+  const wageInputRef = useRef();
+
+  const minimumWage = 15000;
 
   const addWorkerHandler = (e) => {
     e.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredWage = wageInputRef.current.value;
     //Trim tam çalışmıyor. İlk eklerken consola yazdırabiliyorum.
-    if(enteredWorkerName.trim().length === 0){
-        setError({
-          title: "İsim alanı zorunludur!",
-          message: "Lütfen bir isim giriniz."
-        });
-        return;
+    if (nameInputRef.current.value.trim().length === 0) {
+      setError({
+        title: "İsim alanı zorunludur!",
+        message: "Lütfen bir isim giriniz.",
+      });
+      return;
     }
-    if(+enteredWage < minimumWage){
-        setError({
-          title:"Maaş alanı zorunludur!",
-          message:`Lütfen ${minimumWage} değerinden daha büyük bir maaş değeri giriniz.`
-        });
-        return;
+    if (+wageInputRef.current.value < minimumWage) {
+      setError({
+        title: "Maaş alanı zorunludur!",
+        message: `Lütfen ${minimumWage} değerinden daha büyük bir maaş değeri giriniz.`,
+      });
+      return;
     }
 
     props.setWorkers((prevState) => [
       {
-        id: Math.floor((Math.random() * 1000)),
-        name: enteredWorkerName,
-        wage: enteredWage
+        id: Math.floor(Math.random() * 1000),
+        name: enteredName,
+        wage: enteredWage,
       },
-      ...prevState
+      ...prevState,
     ]);
-    setEnteredWorkerName("");
-  setEnteredWage("");
-  }
-  
-const errorHandler = () => {
+    nameInputRef.current.value = "";
+    wageInputRef.current.value = "";
+  };
+
+  const errorHandler = () => {
     setError(null);
-}
-  
-return (
-    <div>
-      
+  };
+
+  return (
+    <Fragment>
       <Card className="mt-7">
-    <form action="" className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
-        <label htmlFor="name" className="font-medium">Çalışan İsmi</label>
-        <input type="text" className="max-w-[40rem] w-full mx-auto" placeholder="Çalışan ismini giriniz" id="name" 
-        onChange={(e) => setEnteredWorkerName(e.target.value)}/>
-        <label htmlFor="wage" className="font-medium">Maaş Miktari</label>
-        <input type="number" className="max-w-[40rem] w-full mx-auto" placeholder="Maaş miktarı yazınız" id="wage" 
-        onChange={(e) => setEnteredWage(e.target.value)}/>
-        <Button className="mt-2 bg-yellow-600" type="submit">Ekle</Button>
-    </form>
-    </Card> 
-    {error && <ErrorModal onConfirm={errorHandler} error={error}>
-        
-        </ErrorModal>}
-    </div> 
-    
-)
-}
-export default AddWorker
+        <form
+          action=""
+          className="flex flex-col gap-y-2"
+          onSubmit={addWorkerHandler}
+        >
+          <label htmlFor="name" className="font-medium">
+            Çalışan İsmi
+          </label>
+          <input
+            type="text"
+            className="max-w-[40rem] w-full mx-auto"
+            placeholder="Çalışan ismini giriniz"
+            id="name"
+            ref={nameInputRef}
+          />
+          <label htmlFor="wage" className="font-medium">
+            Maaş Miktari
+          </label>
+          <input
+            type="number"
+            className="max-w-[40rem] w-full mx-auto"
+            placeholder="Maaş miktarı yazınız"
+            id="wage"
+            ref={wageInputRef}
+          />
+          <Button className="mt-2 bg-yellow-600" type="submit">
+            Ekle
+          </Button>
+        </form>
+      </Card>
+      {error && (
+        <ErrorModal onConfirm={errorHandler} error={error}></ErrorModal>
+      )}
+    </Fragment>
+  );
+};
+export default AddWorker;
