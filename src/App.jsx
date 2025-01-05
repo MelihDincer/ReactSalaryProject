e45321846b2687cfbdd55202890a6c774ab8bf06
Melih Dincer <m_dincer41@hotmail.com>
2025-01-05 23:47:55 +0300

    Localstorage işlemleri tamamlandı.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddWorker from "./components/Workers/AddWorker";
 import WorkerList from "./components/Workers/WorkerList";
-import React from "react";
 
 function App() {
-  const [workers, setWorkers] = useState([])
+  const [workers, setWorkers] = useState(localStorage.getItem("workers") ? JSON.parse(localStorage.getItem("workers")) : [])
+
+  useEffect(() => {
+    localStorage.setItem("workers", JSON.stringify(workers));
+  }, [workers])
+
   return (
     <React.Fragment>
       <h1 className="text-white text-center mt-5 text-2xl">MAAŞ OTOMASYONU</h1>
-     <AddWorker setWorkers = {setWorkers}></AddWorker>
-     <WorkerList workers={workers} setWorkers = {setWorkers}></WorkerList>
-     </React.Fragment>
+      <AddWorker setWorkers={setWorkers}></AddWorker>
+      <WorkerList workers={workers} setWorkers={setWorkers}></WorkerList>
+    </React.Fragment>
   );
 }
 
